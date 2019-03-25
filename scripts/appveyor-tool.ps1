3b0d6a7d810f31962cee9877105d4eedc4f22e0c
Jeroen Ooms <jeroenooms@gmail.com>
2019-03-25 10:00:54 +0100

    update miktex

--- scripts/appveyor-tool.ps1
+++ scripts/appveyor-tool.ps1
@@ -1,5 +1,5 @@
 $CRAN = "https://cloud.r-project.org"
-$MIKTEX_MIRROR = "https://miktex.org/download/ctan/systems/win32/miktex/setup/windows-x64/basic-miktex-2.9.6942-x64.exe"
+$MIKTEX_MIRROR = "https://miktex.org/download/ctan/systems/win32/miktex/setup/windows-x64/basic-miktex-2.9.7021-x64.exe"
 
 $PERL_MIRROR = "http://strawberryperl.com/download/5.28.1.1/strawberry-perl-5.28.1.1-64bit-portable.zip"
 #$PERL_MIRROR = "https://ftp.bit.nl/xbmc/build-deps/win32/strawberry-perl-5.28.0.1-64bit-portable.zip"
