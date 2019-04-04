84fad6559a229a64ccfe11c92ca01233bffbc6d0
Jeroen Ooms <jeroenooms@gmail.com>
2019-04-04 13:37:54 +0200

    Cleanup old patches

--- scripts/build.bat
+++ scripts/build.bat
@@ -66,11 +66,8 @@ xcopy /s "%SOURCEDIR%\cairo\include\cairo" "%R_HOME%\cairo\win64"
 cd %R_HOME%
 patch -p1 -i %SOURCEDIR%\patches\cairo.diff
 
-:: Fixed in r-devel (3.6):
-patch -N -p1 -i %SOURCEDIR%\patches\cranextra.diff
 :: patch -p1 -i %SOURCEDIR%\patches\objdump.diff
 patch -p1 -i %SOURCEDIR%\patches\shortcut.diff
-:: patch -p1 -i %SOURCEDIR%\patches\cairopath.diff
 
 :: Switch dir
 cd %R_HOME%/src/gnuwin32
